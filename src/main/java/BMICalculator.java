9012c6e27df4280342ab6dce2b6d822793356e60
Sebastian Lubiecki <sebastian.lubiecki@gmail.com>
2018-12-08 11:20:46 +0100

    RedRedRed

--- src/main/java/BMICalculator.java
+++ src/main/java/BMICalculator.java
@@ -1,13 +1,41 @@
-public class BMICalculator implements Calculate{
+public class BMICalculator implements Calculate {
 
+    private double weight;
+    private double height;
+
+    public BMICalculator(double weight, double height) {
+        setWeight(weight);
+        setHeight(height);
+    }
+
+    public BMICalculator() {
+    }
 
     @Override
     public String calculate() {
+
         return null;
     }
 
     @Override
     public String interpret() {
         return null;
+    }
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 }
--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -1,2 +1,74 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class BMICalculatorTest {
+
+
+    private BMICalculator bmiCalculator;
+
+
+    @Before
+    public void setBmiCalculator() {
+        this.bmiCalculator = new BMICalculator();
+    }
+
+    @Test
+    public void BMICalculateTestWithParametersZeroTest() {
+        bmiCalculator.setHeight(0);
+        bmiCalculator.setWeight(0);
+        try {
+            bmiCalculator.calculate();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "Wrong parameters");
+        }
+    }
+
+    @Test
+    public void BMICalculateTestWithParameterHeightLowerThenZeroTest() {
+        bmiCalculator.setHeight(-1);
+        bmiCalculator.setWeight(5);
+        try {
+            bmiCalculator.calculate();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "Wrong parameters");
+        }
+    }
+
+    @Test
+    public void BMICalculateTestWithParameterWeightLowerThenZeroTest() {
+        bmiCalculator.setHeight(1);
+        bmiCalculator.setWeight(-5);
+        try {
+            bmiCalculator.calculate();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "Wrong parameters");
+        }
+    }
+
+    @Test
+    public void BMIInterpretWithUnderweightTest() {
+        bmiCalculator.setHeight(200);
+        bmiCalculator.setWeight(50);
+        assertEquals(bmiCalculator.interpret(), "Underweight");
+    }
+
+    @Test
+    public void BMIInterpretWithNormalWeightTest() {
+        bmiCalculator.setHeight(180);
+        bmiCalculator.setWeight(70);
+        assertEquals(bmiCalculator.interpret(), "NormalWeight");
+    }
+
+    @Test
+    public void BMIInterpretWithOverWeightTest() {
+        bmiCalculator.setHeight(150);
+        bmiCalculator.setWeight(200);
+        assertEquals(bmiCalculator.interpret(), "OverWeight");
+    }
 }
