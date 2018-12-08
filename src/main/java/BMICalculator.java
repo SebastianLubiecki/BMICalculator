a5b4c334f7b9ff95ff0b4812c7b968d73a40caf4
Sebastian Lubiecki <sebastian.lubiecki@gmail.com>
2018-12-08 10:17:17 +0100

    before red

---
+++ src/main/java/BMICalculator.java
@@ -0,0 +1,13 @@
+public class BMICalculator implements Calculate{
+
+
+    @Override
+    public String calculate() {
+        return null;
+    }
+
+    @Override
+    public String interpret() {
+        return null;
+    }
+}
---
+++ src/main/java/Calculate.java
@@ -0,0 +1,5 @@
+public interface Calculate {
+
+     String calculate();
+     String interpret ();
+}
